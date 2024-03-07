e91a2920b21527c240376bdc0790bf5bc4f6c6cd
Gayan65 <gayan65728@yahoo.com>
2024-03-07 14:46:30 +0200

    git activity page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./components/Static/Home";
 import TasksPage from "./components/Static/TasksPage";
 import Performance from "./components/Static/Performance";
 import Notifications from "./components/Static/Notifications";
+import ActivityPage from "./components/Static/ActivityPage";
 
 function App() {
     return (
@@ -19,6 +20,10 @@ function App() {
                     <Route
                         path="/notifications"
                         element={<Notifications />}
+                    ></Route>
+                    <Route
+                        path="/activities"
+                        element={<ActivityPage />}
                     ></Route>
                 </Routes>
             </BrowserRouter>
---
+++ frontend/src/components/Static/ActivityPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActivityPage = () => {
+    return <div>This is activity page</div>;
+};
+
+export default ActivityPage;

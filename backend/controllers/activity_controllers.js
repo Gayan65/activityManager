6ef8dc349dcbddefd8488884181cabba2416843a
Gayan65 <gayan65728@yahoo.com>
2024-03-07 20:27:02 +0200

    BACKEND- API for get an activity from an activity id, add

--- backend/controllers/activity_controllers.js
+++ backend/controllers/activity_controllers.js
@@ -1,7 +1,10 @@
 import express from "express";
 
 //IMPORT ALL THE FUNCTIONS FROM SERVICES
-import { getAllActivities } from "../services/activity_services.js";
+import {
+    getActivityFromId,
+    getAllActivities,
+} from "../services/activity_services.js";
 
 const activityRouter = express.Router();
 
@@ -13,6 +16,23 @@ activityRouter.get("/all", async (req, res) => {
             success: true,
             message: "activities found successfully!",
             activities: activities.rows,
+        });
+    } else {
+        res.status(200).json({
+            success: false,
+            message: "Activity cannot be found",
+        });
+    }
+});
+
+//API GET AN ACTIVITY FROM AN ACTIVITY ID (activity detail page)
+activityRouter.get("/:id", async (req, res) => {
+    const activity = await getActivityFromId(req.params.id);
+    if (activity.rowCount > 0) {
+        res.status(200).json({
+            success: true,
+            message: "activity found successfully!",
+            activity: activity.rows,
         });
     } else {
         res.status(200).json({
--- backend/services/activity_services.js
+++ backend/services/activity_services.js
@@ -5,3 +5,11 @@ export const getAllActivities = async () => {
     const res = await db.query("SELECT * FROM activity");
     return res;
 };
+
+//GET AN ACTIVITY FROM THE ACTIVITY_ID - SQL
+export const getActivityFromId = async (activityId) => {
+    const res = await db.query(
+        `SELECT * FROM activity WHERE activity.id = ${activityId}`
+    );
+    return res;
+};

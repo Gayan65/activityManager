18ebe4b6cc70cd9794244c2a9848d145e84e5031
Gayan65 <gayan65728@yahoo.com>
2024-03-07 10:42:29 +0200

    CURRENT TASKS- loader spinng, mapping taks, add

--- frontend/src/components/Static/Home.tsx
+++ frontend/src/components/Static/Home.tsx
@@ -1,16 +1,64 @@
-import React from "react";
-import { Container, Nav } from "react-bootstrap";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Container, Nav, Spinner, ListGroup, Card } from "react-bootstrap";
 
 const Home = () => {
+    //DEFINE THE TASK OBJ
+    interface Task {
+        id: number;
+        name: string;
+    }
+
+    const [activeTasks, setActiveTasks] = useState<Task[] | null>(null);
+    const [loader, setLoader] = useState(true);
+
+    //CALLING APIS ONCE PAGE LOADED
+    useEffect(() => {
+        //(GETTING ALL ACTIVE TASKS AND SET IT TO STATE)
+        axios
+            .get("http://localhost:4000/task/active")
+            .then((response) => {
+                if (response.data.success) {
+                    setActiveTasks(response.data.tasks as Task[]);
+                    setLoader(false);
+                } else {
+                    setActiveTasks(null);
+                    setLoader(false);
+                }
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
         <Container>
             <Nav className="flex-column">
-                {/* LINKS TO PAGES */}
+                {/* LINKS TO PAGES START*/}
                 <Nav.Link href="/tasks">Tasks</Nav.Link>
                 <Nav.Link href="/performance">Performance</Nav.Link>
                 <Nav.Link href="/notifications">Notifications</Nav.Link>
+                {/* LINKS TO PAGES END*/}
             </Nav>
-            <Container>{/* CURRENT TASKS VIEW */}</Container>
+            <Container>
+                {/* CURRENT TASKS VIEW START*/}
+                <Card style={{ width: "18rem" }}>
+                    <Card.Body>
+                        <Card.Title>Active Tasks</Card.Title>
+                        {activeTasks && activeTasks.length > 0 ? (
+                            <ListGroup>
+                                {activeTasks.map((task) => (
+                                    <ListGroup.Item key={task.id}>
+                                        {task.name}
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                        ) : (
+                            <p>No active tasks available.</p>
+                        )}
+                        {loader && <Spinner animation="border" />}
+                    </Card.Body>
+                </Card>
+                {/* CURRENT TASKS VIEW ENDS*/}
+            </Container>
             <Container>{/* PERFORMANCE VIEW */}</Container>
             <Container>{/* NOTIFICATIONS TASKS VIEW */}</Container>
         </Container>

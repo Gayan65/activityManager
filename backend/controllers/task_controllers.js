5b60c1d8d589acfbb9a31f4b9282a7f70902cbbd
Gayan65 <gayan65728@yahoo.com>
2024-03-07 06:05:09 +0200

    API for get_all_active_tasks add

--- backend/controllers/task_controllers.js
+++ backend/controllers/task_controllers.js
@@ -1,13 +1,30 @@
 import express from "express";
 
 //IMPORT ALL THE FUNCTIONS FROM SERVICES
-import { getAllTasks } from "../services/task_services.js";
+import { getAllTasks, getAllActiveTasks } from "../services/task_services.js";
 
 const taskRouter = express.Router();
 
-//API GET ALL TASKS
+//API GET ALL TASKS - NEW, IN PROCESS, DONE, CANCELLED (Manage Existing Task page)
 taskRouter.get("/all", async (req, res) => {
     const tasks = await getAllTasks();
+    if (tasks.rowCount > 0) {
+        res.status(200).json({
+            success: true,
+            message: "tasks found successfully!",
+            tasks: tasks.rows,
+        });
+    } else {
+        res.status(200).json({
+            success: false,
+            message: "Tasks cannot be found",
+        });
+    }
+});
+
+//API GET ALL ACTIVE TASKS - NEW, IN PROCESS (Main Dashboard View)
+taskRouter.get("/active", async (req, res) => {
+    const tasks = await getAllActiveTasks();
     if (tasks.rowCount > 0) {
         res.status(200).json({
             success: true,
--- backend/services/task_services.js
+++ backend/services/task_services.js
@@ -5,3 +5,11 @@ export const getAllTasks = async () => {
     const res = await db.query("SELECT * FROM task");
     return res;
 };
+
+//GET ALL ACTIVE TASKS - SQL
+export const getAllActiveTasks = async () => {
+    const res = await db.query(
+        "SELECT * FROM task WHERE task.status = 1 OR task.status = 2;"
+    );
+    return res;
+};

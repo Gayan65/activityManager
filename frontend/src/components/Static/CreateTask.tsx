8d788cf078501232d2d95a7026fd09162b37c6e2
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-03-11 20:29:49 +0200

    Basic form for CREATETASK add, tags adding not handdled yet

--- frontend/src/components/Static/CreateTask.tsx
+++ frontend/src/components/Static/CreateTask.tsx
@@ -1,45 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Form, Button, Card } from "react-bootstrap";
 
 const CreateTask = () => {
+  //FORM DATA ONCE PAGE LOAD (only for task and NO TAGS INVOLVED!)
+  const defaultFormData = {
+    name: "",
+    content: "",
+    startdate: "",
+    enddate: "",
+    activityid: "",
+    status: 1,
+  };
+
+  //FORM DATA SET TO THE STATE
+  const [formData, setFormData] = useState(defaultFormData);
+
+  //HANDLE INPUT CHANGE
+  const onChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    setFormData((prevSate) => ({
+      ...prevSate,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  //HANDLE FORM SUBMIT
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+
+    setFormData(defaultFormData);
+  };
+
   return (
     <Container className="mt-5">
       <Card style={{ width: "35rem" }}>
         <Card.Body>
           <Card.Title>Task Create </Card.Title>
-          <Form>
+          <Form method="POST" onSubmit={(e) => onSubmit(e)}>
             {/* TASK NAME START */}
             <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Name" />
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                name="name"
+                onChange={onChange}
+                value={formData.name}
+                required
+              />
             </Form.Group>
             {/* TASK NAME END */}
 
             {/* TASK CONTENT START */}
             <Form.Group className="mb-3" controlId="formBasicContent">
               <Form.Label>Content</Form.Label>
-              <Form.Control as="textarea" rows={5} placeholder="Content" />
+              <Form.Control
+                as="textarea"
+                rows={5}
+                placeholder="Content"
+                name="content"
+                onChange={onChange}
+                value={formData.content}
+                required
+              />
             </Form.Group>
             {/* TASK CONTENT END */}
 
             {/* TASK STAR DATE START */}
             <Form.Group className="mb-3" controlId="formBasicDate">
               <Form.Label>Start Date</Form.Label>
-              <Form.Control type="date" placeholder="Start Date" />
+              <Form.Control
+                type="date"
+                placeholder="Start Date"
+                name="startdate"
+                onChange={onChange}
+                value={formData.startdate}
+                required
+              />
             </Form.Group>
             {/* TASK START DATE END */}
 
             {/* TASK END DATE START */}
             <Form.Group className="mb-3" controlId="formBasicDate">
               <Form.Label>End Date</Form.Label>
-              <Form.Control type="date" placeholder="End Date" />
+              <Form.Control
+                type="date"
+                placeholder="End Date"
+                value={formData.enddate}
+                name="enddate"
+                onChange={onChange}
+                required
+              />
             </Form.Group>
             {/* TASK END DATE END */}
 
             {/* TASK ACTIVITY CHOICE START */}
             <Form.Group className="mb-3" controlId="formBasicSelect">
               <Form.Label>Select your Activity</Form.Label>
-              <Form.Control as="select">
+              <Form.Control
+                as="select"
+                name="activityid"
+                value={formData.activityid}
+                onChange={onChange}
+                required
+              >
                 <option>Option 1</option>
                 <option>Option 2</option>
                 <option>Option 3</option>

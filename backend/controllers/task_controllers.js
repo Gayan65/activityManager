b5ba448cb542648ee4319299e04028ca58505301
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-03-12 16:19:12 +0200

    task add api works, tag has not being handled

--- backend/controllers/task_controllers.js
+++ backend/controllers/task_controllers.js
@@ -1,81 +1,100 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 //IMPORT ALL THE FUNCTIONS FROM SERVICES
 import {
-    getAllTasks,
-    getAllActiveTasks,
-    getTaskFromId,
-    getTaskFromActivityId,
+  getAllTasks,
+  getAllActiveTasks,
+  getTaskFromId,
+  getTaskFromActivityId,
+  createTask,
 } from "../services/task_services.js";
 
 const taskRouter = express.Router();
+taskRouter.use(bodyParser.urlencoded({ extended: false }));
 
 //API GET ALL TASKS - NEW, IN PROCESS, DONE, CANCELLED (Manage Existing Task page)
 taskRouter.get("/all", async (req, res) => {
-    const tasks = await getAllTasks();
-    if (tasks.rowCount > 0) {
-        res.status(200).json({
-            success: true,
-            message: "tasks found successfully!",
-            tasks: tasks.rows,
-        });
-    } else {
-        res.status(200).json({
-            success: false,
-            message: "Tasks cannot be found",
-        });
-    }
+  const tasks = await getAllTasks();
+  if (tasks.rowCount > 0) {
+    res.status(200).json({
+      success: true,
+      message: "tasks found successfully!",
+      tasks: tasks.rows,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      message: "Tasks cannot be found",
+    });
+  }
 });
 
 //API GET ALL ACTIVE TASKS - NEW, IN PROCESS (Main Dashboard View)
 taskRouter.get("/active", async (req, res) => {
-    const tasks = await getAllActiveTasks();
-    if (tasks.rowCount > 0) {
-        res.status(200).json({
-            success: true,
-            message: "tasks found successfully!",
-            tasks: tasks.rows,
-        });
-    } else {
-        res.status(200).json({
-            success: false,
-            message: "Tasks cannot be found",
-        });
-    }
+  const tasks = await getAllActiveTasks();
+  if (tasks.rowCount > 0) {
+    res.status(200).json({
+      success: true,
+      message: "tasks found successfully!",
+      tasks: tasks.rows,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      message: "Tasks cannot be found",
+    });
+  }
 });
 
 //API GET A TASK FROM A TASK ID (tasks detail page)
 taskRouter.get("/:id", async (req, res) => {
-    const task = await getTaskFromId(req.params.id);
-    if (task.rowCount > 0) {
-        res.status(200).json({
-            success: true,
-            message: "task found successfully!",
-            activity: task.rows,
-        });
-    } else {
-        res.status(200).json({
-            success: false,
-            message: "task cannot be found",
-        });
-    }
+  const task = await getTaskFromId(req.params.id);
+  if (task.rowCount > 0) {
+    res.status(200).json({
+      success: true,
+      message: "task found successfully!",
+      activity: task.rows,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      message: "task cannot be found",
+    });
+  }
 });
 
 //API GET ALL TASKS FROM AN ACTIVITY ID (activity -> task -> detail page)
 taskRouter.get("/activity/:id", async (req, res) => {
-    const tasks = await getTaskFromActivityId(req.params.id);
-    if (tasks.rowCount > 0) {
-        res.status(200).json({
-            success: true,
-            message: "tasks found successfully!",
-            tasks: tasks.rows,
-        });
-    } else {
-        res.status(200).json({
-            success: false,
-            message: "tasks cannot be found",
-        });
-    }
+  const tasks = await getTaskFromActivityId(req.params.id);
+  if (tasks.rowCount > 0) {
+    res.status(200).json({
+      success: true,
+      message: "tasks found successfully!",
+      tasks: tasks.rows,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      message: "tasks cannot be found",
+    });
+  }
+});
+
+//API ADD NEW TASK
+taskRouter.post("/create", async (req, res) => {
+  const { name, content, startdate, enddate, activityid, status, tags } =
+    req.body;
+  const task = await createTask(
+    name,
+    content,
+    startdate,
+    enddate,
+    activityid,
+    status,
+    tags
+  );
+  console.log(task);
 });
 
 export default taskRouter;
--- backend/services/task_services.js
+++ backend/services/task_services.js
@@ -2,30 +2,47 @@ import db from "../db/db.js";
 
 //GET ALL TASKS - SQL
 export const getAllTasks = async () => {
-    const res = await db.query(
-        "SELECT task.id, task.name, task.content, task.startdate, task.enddate, status.title AS status, activity.title,  ARRAY_AGG(tag.name) AS tagnames FROM task LEFT JOIN status ON task.status = status.id LEFT JOIN activity ON task.activityid = activity.id LEFT JOIN tagtask ON task.id = tagtask.taskid LEFT JOIN tag ON tagtask.tagid = tag.id GROUP BY task.id, status.title, activity.title"
-    );
-    return res;
+  const res = await db.query(
+    "SELECT task.id, task.name, task.content, task.startdate, task.enddate, status.title AS status, activity.title,  ARRAY_AGG(tag.name) AS tagnames FROM task LEFT JOIN status ON task.status = status.id LEFT JOIN activity ON task.activityid = activity.id LEFT JOIN tagtask ON task.id = tagtask.taskid LEFT JOIN tag ON tagtask.tagid = tag.id GROUP BY task.id, status.title, activity.title"
+  );
+  return res;
 };
 
 //GET ALL ACTIVE TASKS - SQL
 export const getAllActiveTasks = async () => {
-    const res = await db.query(
-        "SELECT task.id, task.name, task.content, task.startdate, task.enddate, status.title AS status, activity.title,  ARRAY_AGG(tag.name) AS tagnames FROM task LEFT JOIN status ON task.status = status.id LEFT JOIN activity ON task.activityid = activity.id LEFT JOIN tagtask ON task.id = tagtask.taskid LEFT JOIN tag ON tagtask.tagid = tag.id WHERE task.status = 1 OR task.status = 2 GROUP BY task.id, status.title, activity.title"
-    );
-    return res;
+  const res = await db.query(
+    "SELECT task.id, task.name, task.content, task.startdate, task.enddate, status.title AS status, activity.title,  ARRAY_AGG(tag.name) AS tagnames FROM task LEFT JOIN status ON task.status = status.id LEFT JOIN activity ON task.activityid = activity.id LEFT JOIN tagtask ON task.id = tagtask.taskid LEFT JOIN tag ON tagtask.tagid = tag.id WHERE task.status = 1 OR task.status = 2 GROUP BY task.id, status.title, activity.title"
+  );
+  return res;
 };
 
 //GET A TASK FROM TASK_ID - SQL
 export const getTaskFromId = async (taskId) => {
-    const res = await db.query(`SELECT * FROM task WHERE task.id = ${taskId}`);
-    return res;
+  const res = await db.query(`SELECT * FROM task WHERE task.id = ${taskId}`);
+  return res;
 };
 
 //GET A TASK FROM TASK_ID - SQL
 export const getTaskFromActivityId = async (activityId) => {
-    const res = await db.query(
-        `SELECT * FROM task WHERE activityid = ${activityId}`
-    );
-    return res;
+  const res = await db.query(
+    `SELECT * FROM task WHERE activityid = ${activityId}`
+  );
+  return res;
+};
+
+//CREATE A TASK - SQL
+export const createTask = async (
+  name,
+  content,
+  startdate,
+  enddate,
+  activityid,
+  status,
+  tags
+) => {
+  const res = await db.query(
+    "INSERT INTO Task (Name, Content, StartDate, EndDate, Status, ActivityId) VALUES ($1, $2, $3, $4, $5, $6)",
+    [name, content, startdate, enddate, activityid, status]
+  );
+  return res;
 };

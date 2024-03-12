825d883ad5b79e92af6bad6e58ccdc9e87523a8f
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-03-12 08:28:17 +0200

    API for get all current activities

--- backend/controllers/activity_controllers.js
+++ backend/controllers/activity_controllers.js
@@ -2,44 +2,62 @@ import express from "express";
 
 //IMPORT ALL THE FUNCTIONS FROM SERVICES
 import {
-    getActivityFromId,
-    getAllActivities,
+  getActivityFromId,
+  getAllActivities,
+  getAllCurrentActivities,
 } from "../services/activity_services.js";
 
 const activityRouter = express.Router();
 
 //API GET ALL ACTIVITIES (Manage all activities page)
 activityRouter.get("/all", async (req, res) => {
-    const activities = await getAllActivities();
-    if (activities.rowCount > 0) {
-        res.status(200).json({
-            success: true,
-            message: "activities found successfully!",
-            activities: activities.rows,
-        });
-    } else {
-        res.status(200).json({
-            success: false,
-            message: "Activity cannot be found",
-        });
-    }
+  const activities = await getAllActivities();
+  if (activities.rowCount > 0) {
+    res.status(200).json({
+      success: true,
+      message: "activities found successfully!",
+      activities: activities.rows,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      message: "Activity cannot be found",
+    });
+  }
+});
+
+//API GET ALL CURRENT ACTIVITIES (in drop down for creating the task page in the frontend)
+activityRouter.get("/current/all", async (req, res) => {
+  const activities = await getAllCurrentActivities();
+  if (activities.rowCount > 0) {
+    res.status(200).json({
+      success: true,
+      message: "activities found successfully!",
+      activities: activities.rows,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      message: "Activity cannot be found",
+    });
+  }
 });
 
 //API GET AN ACTIVITY FROM AN ACTIVITY ID (activity detail page)
 activityRouter.get("/:id", async (req, res) => {
-    const activity = await getActivityFromId(req.params.id);
-    if (activity.rowCount > 0) {
-        res.status(200).json({
-            success: true,
-            message: "activity found successfully!",
-            activity: activity.rows,
-        });
-    } else {
-        res.status(200).json({
-            success: false,
-            message: "Activity cannot be found",
-        });
-    }
+  const activity = await getActivityFromId(req.params.id);
+  if (activity.rowCount > 0) {
+    res.status(200).json({
+      success: true,
+      message: "activity found successfully!",
+      activity: activity.rows,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      message: "Activity cannot be found",
+    });
+  }
 });
 
 //NEW API HERE
--- backend/services/activity_services.js
+++ backend/services/activity_services.js
@@ -2,16 +2,24 @@ import db from "../db/db.js";
 
 //GET ALL ACTIVITIES - SQL
 export const getAllActivities = async () => {
-    const res = await db.query(
-        "SELECT activity.id, activity.title, activity.description, activity.url, activity.startdate, activity.enddate, status.title AS status, activitytype.name AS activitytype, ARRAY_AGG(tag.name) AS tagnames FROM activity LEFT JOIN status ON activity.status = status.id LEFT JOIN activitytype ON activity.activitytype = activitytype.id LEFT JOIN tagactivity ON activity.id = tagactivity.activityid LEFT JOIN tag ON tagactivity.tagid = tag.id GROUP BY activity.id, status.title, activitytype.name"
-    );
-    return res;
+  const res = await db.query(
+    "SELECT activity.id, activity.title, activity.description, activity.url, activity.startdate, activity.enddate, status.title AS status, activitytype.name AS activitytype, ARRAY_AGG(tag.name) AS tagnames FROM activity LEFT JOIN status ON activity.status = status.id LEFT JOIN activitytype ON activity.activitytype = activitytype.id LEFT JOIN tagactivity ON activity.id = tagactivity.activityid LEFT JOIN tag ON tagactivity.tagid = tag.id GROUP BY activity.id, status.title, activitytype.name"
+  );
+  return res;
+};
+
+//GET ALL ACTIVITIES - SQL
+export const getAllCurrentActivities = async () => {
+  const res = await db.query(
+    "SELECT * FROM activity WHERE activity.status = 1 OR activity.status = 2"
+  );
+  return res;
 };
 
 //GET AN ACTIVITY FROM THE ACTIVITY_ID - SQL
 export const getActivityFromId = async (activityId) => {
-    const res = await db.query(
-        `SELECT * FROM activity WHERE activity.id = ${activityId}`
-    );
-    return res;
+  const res = await db.query(
+    `SELECT * FROM activity WHERE activity.id = ${activityId}`
+  );
+  return res;
 };

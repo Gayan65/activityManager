1c59f37cfb6cedfd532d4dd63a4988cb0d6ac8f6
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-03-13 10:00:48 +0200

    create task frontend axios req add

--- frontend/src/components/Static/CreateTask.tsx
+++ frontend/src/components/Static/CreateTask.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import qs from "qs";
 import { Container, Form, Button, Card, Alert } from "react-bootstrap";
 
 const CreateTask = () => {
@@ -17,16 +18,14 @@ const CreateTask = () => {
     activityid: "",
     status: 1,
     tags: "",
-
-    //NOTE! - tags are being handled by separate special API it add all the task details as well as tags info simultaneously!
   };
 
-  //FORM DATA SET TO THE STATE
+  //-------------FORM DATA SET TO THE STATE-----------------
   const [formData, setFormData] = useState(defaultFormData); // FORM DATA
   const [activityData, setActivityData] = useState<Activity[] | null>(null); // ACTIVITY DATA
   const [errorMessage, setErrorMessage] = useState("");
 
-  //HANDLE INPUT CHANGE
+  //--------------HANDLE INPUT CHANGE-----------------
   const onChange = (
     e: React.ChangeEvent<
       HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
@@ -38,7 +37,7 @@ const CreateTask = () => {
     }));
   };
 
-  //HANDLE FORM SUBMIT
+  //--------------HANDLE FORM SUBMIT-------------------
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -52,7 +51,18 @@ const CreateTask = () => {
       setErrorMessage("Please check the start date");
       return;
     }
-    console.log(formData);
+
+    //Making a query string
+    const data = qs.stringify(formData);
+
+    //Calling axios to send the data to api
+    axios
+      .post("http://localhost:4000/task/create", data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
+
     setErrorMessage("Data Add successfully");
 
     setFormData(defaultFormData);
@@ -63,7 +73,7 @@ const CreateTask = () => {
     }, 3000);
   };
 
-  //GET API DATA ONCE LOAD THE PAGE
+  //--------------GET API DATA ONCE LOAD THE PAGE-----------------------
   useEffect(() => {
     //GET CURRENT ACTIVITIES FOR FETCHING THE DROPDOWN UNDER ACTIVITIES IN THE FORM
     axios

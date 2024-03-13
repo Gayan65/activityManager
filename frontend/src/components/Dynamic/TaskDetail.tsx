dc7138b6ebca34a21e1511dd2031c1938166918e
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-03-13 12:38:35 +0200

    delete task acordin add

--- frontend/src/components/Dynamic/TaskDetail.tsx
+++ frontend/src/components/Dynamic/TaskDetail.tsx
@@ -1,48 +1,86 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
-import { Container, Spinner, Card } from "react-bootstrap";
+import {
+  Container,
+  Spinner,
+  Card,
+  Accordion,
+  Form,
+  Button,
+} from "react-bootstrap";
 
 const TaskDetail = () => {
-    //DEFINE THE TASK OBJ
-    interface Task {
-        id: number;
-        name: string;
-    }
+  //DEFINE THE TASK OBJ
+  interface Task {
+    id: number;
+    name: string;
+  }
 
-    //USE PARAMS TO FETCH THE ID FOR AXIOS CALL
-    const params = useParams();
+  //USE PARAMS TO FETCH THE ID FOR AXIOS CALL
+  const params = useParams();
 
-    //USE FOR NAVIGATING TO URLS
-    const navigate = useNavigate();
+  //USE FOR NAVIGATING TO URLS
+  const navigate = useNavigate();
 
-    const [task, setTask] = useState<Task | null>(null);
-    const [loader, setLoader] = useState(true);
+  const [task, setTask] = useState<Task | null>(null);
+  const [loader, setLoader] = useState(true);
 
-    //CALLING BACKEND API TO FETCH THE DATA
-    useEffect(() => {
-        axios
-            .get(`http://localhost:4000/task/${params.id}`)
-            .then((response) => {
-                setTask(response.data.activity[0] as Task);
-                setLoader(false);
-            })
-            .catch((err) => navigate("/error"));
-    }, [navigate, params.id]);
+  //CALLING BACKEND API TO FETCH THE DATA
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/task/${params.id}`)
+      .then((response) => {
+        setTask(response.data.activity[0] as Task);
+        setLoader(false);
+      })
+      .catch((err) => navigate("/error"));
+  }, [navigate, params.id]);
 
-    return (
-        <Container>
-            Task Detail Page
-            {task && (
-                <Card style={{ width: "18rem" }}>
-                    <Card.Body>
-                        <Card.Title>{task.name}</Card.Title>
-                        {loader && <Spinner animation="border" />}
-                    </Card.Body>
-                </Card>
-            )}
-        </Container>
-    );
+  return (
+    <Container>
+      Task Detail Page
+      {task && (
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>{task.name}</Card.Title>
+            {loader && <Spinner animation="border" />}
+          </Card.Body>
+        </Card>
+      )}
+      {task && (
+        <div>
+          {/* DELETE TASK VIEW START*/}
+          <Card style={{ width: "18rem" }} className="mt-5">
+            <Card.Body>
+              <Accordion>
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Delete Task</Accordion.Header>
+                  <Accordion.Body>
+                    Danger Zone: this section can be performed a delete activity
+                    of a task.
+                    <Form>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicTaskTile"
+                      >
+                        <Form.Control type="text" placeholder="Task title" />
+                      </Form.Group>
+                      <Button variant="danger" type="submit">
+                        Delete
+                      </Button>
+                    </Form>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Card.Body>
+          </Card>
+
+          {/* DELETE TASK VIEW END*/}
+        </div>
+      )}
+    </Container>
+  );
 };
 
 export default TaskDetail;
--- frontend/src/components/Static/TasksPage.tsx
+++ frontend/src/components/Static/TasksPage.tsx
@@ -3,61 +3,57 @@ import axios from "axios";
 import { Container, Spinner, ListGroup, Card } from "react-bootstrap";
 
 const TasksPage = () => {
-    //DEFINE THE TASK OBJ
-    interface Task {
-        id: number;
-        name: string;
-    }
+  //DEFINE THE TASK OBJ
+  interface Task {
+    id: number;
+    name: string;
+  }
 
-    const [allTasks, setAllTasks] = useState<Task[] | null>(null);
-    const [loader, setLoader] = useState(true);
+  const [allTasks, setAllTasks] = useState<Task[] | null>(null);
+  const [loader, setLoader] = useState(true);
 
-    //CALLING APIS ONCE PAGE LOADED
-    useEffect(() => {
-        //(GETTING ALL TASKS AND SET IT TO STATE)
-        axios
-            .get("http://localhost:4000/task/all")
-            .then((response) => {
-                if (response.data.success) {
-                    setAllTasks(response.data.tasks as Task[]);
-                    setLoader(false);
-                } else {
-                    setAllTasks(null);
-                    setLoader(false);
-                }
-            })
-            .catch((err) => console.log(err));
-    }, []);
+  //CALLING APIS ONCE PAGE LOADED
+  useEffect(() => {
+    //(GETTING ALL TASKS AND SET IT TO STATE)
+    axios
+      .get("http://localhost:4000/task/all")
+      .then((response) => {
+        if (response.data.success) {
+          setAllTasks(response.data.tasks as Task[]);
+          setLoader(false);
+        } else {
+          setAllTasks(null);
+          setLoader(false);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <Container>
-            {/* MAP THE EXISTING TASKS, NEW TASKS */}
-            Task Manage Page
-            {/* ALL TASKS VIEW START*/}
-            <Card style={{ width: "18rem" }}>
-                <Card.Body>
-                    <Card.Title>Active Tasks</Card.Title>
-                    {allTasks && allTasks.length > 0 ? (
-                        <ListGroup>
-                            {allTasks.map((task) => (
-                                <ListGroup.Item
-                                    key={task.id}
-                                    action
-                                    href={`/task/${task.id}`}
-                                >
-                                    {task.name}
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
-                    ) : (
-                        <p>No tasks available.</p>
-                    )}
-                    {loader && <Spinner animation="border" />}
-                </Card.Body>
-            </Card>
-            {/* ALL TASKS VIEW END*/}
-        </Container>
-    );
+  return (
+    <Container>
+      {/* MAP THE EXISTING TASKS, NEW TASKS */}
+      Task Manage Page
+      {/* ALL TASKS VIEW START*/}
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Active Tasks</Card.Title>
+          {allTasks && allTasks.length > 0 ? (
+            <ListGroup>
+              {allTasks.map((task) => (
+                <ListGroup.Item key={task.id} action href={`/task/${task.id}`}>
+                  {task.name}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          ) : (
+            <p>No tasks available.</p>
+          )}
+          {loader && <Spinner animation="border" />}
+        </Card.Body>
+      </Card>
+      {/* ALL TASKS VIEW END*/}
+    </Container>
+  );
 };
 
 export default TasksPage;

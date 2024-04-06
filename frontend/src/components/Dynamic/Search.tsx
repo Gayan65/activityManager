093029b842e25143bc56c6b658654a959acddb03
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-04-06 18:09:14 +0300

    fronend form add

--- frontend/src/components/Dynamic/Search.tsx
+++ frontend/src/components/Dynamic/Search.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Form, Button, Card, Container } from "react-bootstrap";
+import qs from "qs";
+import axios from "axios";
+import { Form, Button, Card, Container, Alert } from "react-bootstrap";
 
 const Search = () => {
   //FORM DATA ONCE PAGE LOAD
@@ -13,6 +15,7 @@ const Search = () => {
   //-------------FORM DATA SET TO THE STATE-----------------
   const [formData, setFormData] = useState(defaultFormData); // FORM DATA
   const [selectedOption, setSelectedOption] = useState<string>("Activity"); //STATE FOR RADIO BUTTONS
+  const [errorMessage, setErrorMessage] = useState("");
 
   //HANDLING RADIO BUTTON EVENTS
   const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,7 +25,9 @@ const Search = () => {
 
   //--------------HANDLE INPUT CHANGE-----------------
   const onChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
   ) => {
     setFormData((prevSate) => ({
       ...prevSate,
@@ -30,66 +35,122 @@ const Search = () => {
     }));
   };
 
-  //ON SUBMIT HERE
+  //--------------HANDLE FORM SUBMIT-------------------
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //ADD A HANDLER FOR CHECK START DATE NO CHECK YET
+    if (formData.startdate > formData.enddate) {
+      setErrorMessage("Please check the date fields");
+      return;
+    }
+
+    //Making a query string
+    const data = qs.stringify(formData);
+
+    //Calling axios to send the data to api
+    console.log(formData);
+    setErrorMessage("Data add successfully");
+
+    setFormData(defaultFormData);
+
+    //THE ERROR MESSAGE WILL BE GONE IN 3S
+    setTimeout(() => {
+      setErrorMessage("");
+    }, 3000);
+  };
 
   return (
     <Container className="mt-5">
       <Card style={{ width: "45rem" }}>
         <Card.Body>
           <Card.Title>Search here</Card.Title>
-          <Form.Check
-            inline
-            label="Activity"
-            name="group1"
-            type="radio"
-            id="radio1"
-            value="Activity"
-            checked={selectedOption === "Activity"}
-            onChange={handleRadioChange}
-          />
-          <Form.Check
-            inline
-            label="Task"
-            name="group1"
-            type="radio"
-            id="radio2"
-            value="Task"
-            checked={selectedOption === "Task"}
-            onChange={handleRadioChange}
-          />
-          <Form>
+
+          <Form method="POST" onSubmit={onSubmit}>
+            <Form.Check
+              inline
+              label="Activity"
+              name="group1"
+              type="radio"
+              id="radio1"
+              value="Activity"
+              checked={selectedOption === "Activity"}
+              onChange={handleRadioChange}
+            />
+            <Form.Check
+              inline
+              label="Task"
+              name="group1"
+              type="radio"
+              id="radio2"
+              value="Task"
+              checked={selectedOption === "Task"}
+              onChange={handleRadioChange}
+            />
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control
+                type="text"
                 className="mb-2"
-                id="inlineFormInput"
                 placeholder="Name"
+                onChange={onChange}
+                name="name"
+                value={formData.name}
               />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicSelect">
-              <Form.Label>Select your Activity</Form.Label>
-              <Form.Control as="select">
-                <option>New</option>
-                <option>In Process</option>
-                <option>Completed</option>
-                <option>Cancelled</option>
-                <option>All</option>
+              <Form.Label>Select Status</Form.Label>
+              <Form.Control
+                as="select"
+                name="status"
+                onChange={onChange}
+                value={formData.status}
+              >
+                <option value={1}>New</option>
+                <option value={2}>In Process</option>
+                <option value={3}>Completed</option>
+                <option value={4}>Cancelled</option>
+                <option value={5}>All</option>
               </Form.Control>
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Start Date</Form.Label>
-              <Form.Control type="date" placeholder="Start Date" />
+              <Form.Control
+                type="date"
+                placeholder="Start Date"
+                name="startdate"
+                onChange={onChange}
+                value={formData.startdate}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>End Date</Form.Label>
-              <Form.Control type="date" placeholder="End Date" />
+              <Form.Control
+                type="date"
+                placeholder="End Date"
+                name="enddate"
+                onChange={onChange}
+                value={formData.enddate}
+              />
             </Form.Group>
 
             <Button type="submit" className="mb-2">
               Search
             </Button>
+            {errorMessage && (
+              <Alert
+                className="mt-3"
+                variant={
+                  errorMessage === "Data add successfully"
+                    ? "success"
+                    : "danger"
+                }
+              >
+                {errorMessage}
+              </Alert>
+            )}
           </Form>
         </Card.Body>
       </Card>

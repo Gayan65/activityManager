ba2fa9be4a054c9d75672f1c903f57c0ef317741
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-04-23 22:09:17 +0300

    performance fronedn add

--- frontend/src/components/Dynamic/Performance.tsx
+++ frontend/src/components/Dynamic/Performance.tsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Container, Card, ListGroup } from "react-bootstrap";
 
 const Performance = () => {
   //DEFINE THE TASK OBJ
   interface TasksPerformance {
-    taskCreated: number;
-    taskCompleted: number;
-    onGoingTasks: number;
+    createTask: number;
+    completedTask: number;
+    onGoingTask: number;
+    canceledTask: number;
   }
 
   const [taskPerformance, setTaskPerformance] =
@@ -16,13 +18,40 @@ const Performance = () => {
     axios
       .get("http://localhost:4000/task/performance/task")
       .then((response) => {
-        setTaskPerformance(response.data);
-        console.log(response.data);
+        setTaskPerformance(response.data as TasksPerformance);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  return <div>Performance</div>;
+  return (
+    <Container>
+      Performance
+      <div>
+        {taskPerformance && (
+          <Card style={{ width: "18rem" }}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                {" "}
+                Nos of created Tasks {taskPerformance.createTask}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {" "}
+                Nos of Ongoing Tasks {taskPerformance.onGoingTask}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {" "}
+                Nos of completed Tasks {taskPerformance.completedTask}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {" "}
+                Nos of canceled Tasks {taskPerformance.canceledTask}
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        )}
+      </div>
+    </Container>
+  );
 };
 
 export default Performance;

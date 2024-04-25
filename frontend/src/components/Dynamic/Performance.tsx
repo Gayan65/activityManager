211ff61e260e201644f27a34feb3f72ecf5f9c95
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-04-25 21:13:31 +0300

    Cart added, for performance section Frontend

--- frontend/src/components/Dynamic/Performance.tsx
+++ frontend/src/components/Dynamic/Performance.tsx
@@ -1,61 +1,60 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Container, Card, ListGroup } from "react-bootstrap";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Performance = () => {
-    //DEFINE THE TASK OBJ
-    interface TasksPerformance {
-        createTask: number;
-        completedTask: number;
-        onGoingTask: number;
-        canceledTask: number;
-    }
+  //DEFINE THE TASK OBJ
+  interface TasksPerformance {
+    createTask: number;
+    completedTask: number;
+    onGoingTask: number;
+    canceledTask: number;
+  }
 
-    const [taskPerformance, setTaskPerformance] =
-        useState<TasksPerformance | null>(null);
+  const options = {};
+  const [taskPerformance, setTaskPerformance] =
+    useState<TasksPerformance | null>(null);
 
-    useEffect(() => {
-        axios
-            .get("http://localhost:4000/task/performance/task")
-            .then((response) => {
-                setTaskPerformance(response.data as TasksPerformance);
-            })
-            .catch((err) => console.log(err));
-    }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/task/performance/task")
+      .then((response) => {
+        setTaskPerformance(response.data as TasksPerformance);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <>
-            Performance
-            <div>
-                {taskPerformance && (
-                    <Card style={{ width: "18rem" }}>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>
-                                {" "}
-                                Nos of created Tasks{" "}
-                                {taskPerformance.createTask}
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                {" "}
-                                Nos of Ongoing Tasks{" "}
-                                {taskPerformance.onGoingTask}
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                {" "}
-                                Nos of completed Tasks{" "}
-                                {taskPerformance.completedTask}
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                {" "}
-                                Nos of canceled Tasks{" "}
-                                {taskPerformance.canceledTask}
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                )}
-            </div>
-        </>
-    );
+  const data = {
+    labels: ["New", "In Process", "Completed", "Canceled"],
+    datasets: [
+      {
+        data: [
+          taskPerformance?.createTask,
+          taskPerformance?.onGoingTask,
+          taskPerformance?.completedTask,
+          taskPerformance?.canceledTask,
+        ],
+        backgroundColor: ["#4b80bd", "#1d2a57", "#1f7d0f", "#6e1505"],
+      },
+    ],
+  };
+
+  return (
+    <>
+      Performance
+      <div>
+        {taskPerformance && (
+          <div>
+            <Doughnut data={data} options={options}></Doughnut>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Performance;
